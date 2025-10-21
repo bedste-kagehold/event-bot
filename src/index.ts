e80857b7b9d20cadcdb890064aad64d12bd0f07c
DiversenSato <diversensato@gmail.com>
2025-10-21 17:51:11 +0200

    feat: add initial implementation of bot

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+import { Client, Events } from 'discord.js';
+import 'dotenv/config';
+
+const client = new Client({ intents: [] });
+
+client.once(Events.ClientReady, (client) => {
+    console.log(chalk.bold.greenBright(`Logged in as ${client.user.tag}`));
+});
+
+process.on('SIGINT', () => {
+    console.log(chalk.bold.yellowBright('Shutting down...'));
+    if (client.isReady())
+        client.destroy().catch((err) => {
+            console.error(chalk.bold.redBright('Error during shutdown:'), err);
+        });
+});
+
+await client.login(process.env.DISCORD_TOKEN);
