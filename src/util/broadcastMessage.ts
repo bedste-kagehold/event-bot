2f9f4246850f0f02123eb878eb7f507390fb0644
Android47 <nicolaifh05@gmail.com>
2025-12-15 17:54:26 +0100

    fix: added broadcast delay

--- src/util/broadcastMessage.ts
+++ src/util/broadcastMessage.ts
@@ -2,6 +2,7 @@ import { client } from '../client.js';
 import fs from 'fs';
 import discordGui from '../discordGui.js';
 import { MessageFlags } from 'discord.js';
+import { waitForSeconds } from '../wait.js';
 
 export async function broadcastMessage(guildId: string, channelId: string, message: string[]) {
     console.log(message);
@@ -30,5 +31,6 @@ export async function broadcastMessage(guildId: string, channelId: string, messa
             components: [Container],
             flags: MessageFlags.IsComponentsV2,
         });
+        await waitForSeconds(10);
     }
 }
